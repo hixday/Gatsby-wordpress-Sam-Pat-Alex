c235ea943559ce17aff6d5d1530dd33771e64a4b
samuel faucher <sam.faucher938@gmail.com>
2019-06-21 21:09:55 -0400

    Push Plugin

---
+++ Project_Manager/classes/Interfaces/IManagerCrud.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Project_manager\Interfaces;
+
+interface IManagerCrud
+{
+    public function create(  );
+    public static function read(  );
+    public function update(  );
+    public function delete(  );
+}
---
+++ Project_Manager/classes/Metaboxes/ManagerFields.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Project_manager\Metaboxes;
+
+class ManagerFields
+{
+    protected $version;
+
+    public function __construct() {
+    
+    }
+}
